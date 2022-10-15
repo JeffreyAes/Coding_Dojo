dbe1a425cb9aba5cf1a51d1433bd626d24369f2f
JeffreyAes <aeschlimanjeffrey@gmail.com>
2022-10-14 19:24:57 -0700

    bouncy_buttons

--- Web_Fundementals/js/Button_Clicker/script.js
+++ Web_Fundementals/js/Button_Clicker/script.js
@@ -5,3 +5,11 @@ function logOut(element) {
 function remove(element) {
     element.remove();
 }
+
+function addShadow(element) {
+    element.classList.add("shadow")
+}
+
+function removeShadow(element) {
+    element.classList.remove("shadow")
+}
