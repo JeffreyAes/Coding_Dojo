f1bce5e3c650fae22f71604cca97c1774f7354dc
JeffreyAes <aeschlimanjeffrey@gmail.com>
2022-10-27 14:55:21 -0700

    tables are fun

---
+++ Python/flask/fundamentals/checker_board/server.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+
+@app.route('/')          # The "@" decorator associates this route with the function immediately following
+def index():
+    return render_template("index.html")
+# @app.route('/4')
+# @app.route('x/y')
+# @app.route(10/10)
+
+# Return the string 'Hello World!' as a response
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.
+
+
+"""
+============= TO GET INSTALL FLASK ==================
+1. pipenv install flask
+2. pipenv install
+
+
+============= TO ACTIVATE SERVER =================
+3. pipenv shell
+"""
