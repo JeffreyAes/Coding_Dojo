6793f8d8cec1d7b2bb84e106aaf1d0018bd40ecf
JeffreyAes <aeschlimanjeffrey@gmail.com>
2022-10-27 17:27:57 -0700

    checkers

--- Python/flask/fundamentals/checker_board/server.py
+++ Python/flask/fundamentals/checker_board/server.py
@@ -3,11 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 
 @app.route('/')          # The "@" decorator associates this route with the function immediately following
-def index():
-    return render_template("index.html")
-# @app.route('/4')
-# @app.route('x/y')
-# @app.route(10/10)
+@app.route('/<int:num2>')
+@app.route('/<int:num1>/<int:num2>')
+@app.route('/<int:num1>/<int:num2>/<color1>/<color2>')
+def index(num1 = 8, num2 = 8, color1 = 'black', color2 = 'red'):
+    return render_template("index.html", num1 = num1, num2 = num2, color1 = color1, color2 = color2)
 
 # Return the string 'Hello World!' as a response
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
