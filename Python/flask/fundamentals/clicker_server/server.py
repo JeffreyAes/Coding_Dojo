7858970b1408db78c68a376457c5d418a1ef0da2
JeffreyAes <aeschlimanjeffrey@gmail.com>
2022-10-29 08:16:49 -0700

    comments

--- Python/flask/fundamentals/clicker_server/server.py
+++ Python/flask/fundamentals/clicker_server/server.py
@@ -5,29 +5,30 @@ app.secret_key = 'meme' # set a secret key for security purposes
 
 @app.route('/')
 def enter_site():
-    if 'num' in session:
-        session['num'] += 1
+    if 'num' in session:    #checking if num is already in session
+        session['num'] += 1 #adds 1 everytime you enter the site
     else:
-        session['num'] = 1
-    return render_template('index.html', num=session['num'])
+        session['num'] = 1  #creates session['num'] which acts like a dictionary, and sets it to 1
+    return render_template('index.html', num=session['num'])    #puts session[num] in a variable called 'num'
 
-@app.route('/count')
+@app.route('/count')    #directed here when the increase button is clicked
 def start_count():
     session['num'] +=1
-    return redirect('/')
+    return redirect('/')    #adds 1 to session['num'], then redirects back to route which also adds 1
+                            #which essentially adds 2 to num
 
-@app.route('/destroy_session')
+@app.route('/destroy_session') #directed here when the reset button is clicked
 def destroy_count():
-    session['num'] = 0
-    return redirect('/')
+    session['num'] = 0  #sets the session['num'] to zero (resetting it)
+    return redirect('/')    #directs you back to the root, adding 1 back to the count
 
-@app.route('/<int:num>')
-def add_count(num):
-    if 'num' in session:
-        session['num'] += num 
+@app.route('/<int:num>')    #user inputs an (integer) to the route
+def add_count(num): #takes from the inputed integer and puts it in the paramater
+    if 'num' in session:    #checks if num is already in session
+        session['num'] += num   #adds the inputed integer to session ['num']
     else:
-        session['num'] = 1
-    return render_template('index.html', num=session['num'])
+        session['num'] = 1  #sets session['num'] to 1
+    return render_template('index.html', num=session['num']) #renders index html, puts session['num'] in variable num
 
 
 
--- Python/flask/fundamentals/great_number_game/server.py
+++ Python/flask/fundamentals/great_number_game/server.py
@@ -3,27 +3,35 @@ import random
 
 app = Flask(__name__)
 # set a secret key for security purposes@app.route('/')
-app.secret_key = 'meme'
+app.secret_key = 'meme'     #required
 
 
 @app.route('/')
-def guess_number():
-    if 'random' in session:
-        session['random'] = session['random']
+def guess_number(): 
+    if 'random' in session:        #IF random was put into a session backpack, set it to itsef
+        session['random'] = session['random']   #this keeps it from creating a new random integer each time it redirects/refresh
     else:
-        session['random'] = random.randint(1, 100)
-    session['answer'] = session['answer']
-    return render_template('index.html')
+        session['random'] = random.randint(1, 100)  #puts the random number in the session backpack
+    session['answer'] = session['answer']   #not sure lol
+    return render_template('index.html')    #renders the html for users to give guesses
 
 
 # checks if guess is correct and redirects back with feedback
-@app.route('/response',  methods=['POST'])
-def check_guess():
-    session['count'] = 0
-    session['guess'] = int(request.form['guess'])
-    answer = ""
-    if session['random'] < session['guess']:
-        answer = "WAYYYYY too big"
+
+@app.route('/response',  methods=['POST'])  #guesses are stored and then sent to here through POST method
+
+def check_guess():  #checking if guess is greater/less than the random integer
+
+    session['count'] = 0    #i think this is why it didn't work
+
+    session['guess'] = int(request.form['guess'])   
+    #taking the information from the POST, grabbing the [guess] key and
+    #putting it in a session['guess'] backpack
+
+    answer = "" #empty string to insert text to, given the conditionals met below
+
+    if session['random'] < session['guess']:    #asking if guess is more or less
+        answer = "WAYYYYY too big"  #puts feed back in empty sting
     elif session['random'] > session['guess']:
         answer = "too small"
     else:
@@ -31,7 +39,7 @@ def check_guess():
         session['random'] = random.randint(1,100)
     session['answer'] = answer
     if session['guess'] != session['random']:
-        session['count'] +=0
+        session['count'] =0
     return redirect('/')
 
 
