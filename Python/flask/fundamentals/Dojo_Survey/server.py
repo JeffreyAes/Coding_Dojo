1d3c48a98a70f82370709b864f868e1f3f06aa09
JeffreyAes <aeschlimanjeffrey@gmail.com>
2022-10-29 07:25:03 -0700

    added comments

--- Python/flask/fundamentals/Dojo_Survey/server.py
+++ Python/flask/fundamentals/Dojo_Survey/server.py
@@ -1,27 +1,27 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session  
 
 app = Flask(__name__)
 
-app.secret_key = 'meme'
+app.secret_key = 'meme' #required for sessions
 
 
-@app.route('/')
+@app.route('/') 
 def index():
-    return render_template('index.html')
+    return render_template('index.html')    #display the form for user to input info
 
-@app.route('/process', methods=["POST"])
-def process():
-    session['name'] = request.form['name']
-    session['location'] = request.form['location']
+@app.route('/process', methods=["POST"])    #once submitted it stores and sends the form through the post method
+def process():                              #the post form creates a dictonary called request.form
+    session['name'] = request.form['name']    #now we put the request.form[key] into a session[key] 
+    session['location'] = request.form['location']    #(put info in backpack for reuse)
     session['language'] = request.form['language']
     session['text_area'] = request.form['text_area']
-    print(request.form)
-    return redirect('/result')
+    print(request.form)   #checking if request.form posted correctly (shouldn't show the input)
+    return redirect('/result')    #directs you to the result route
 
 @app.route('/result')
 def result():
 
-    return render_template('result.html')
+    return render_template('result.html')    #displays the post request information in html table
 
 if __name__ == "__main__":
     app.run(debug=True)
