dec53b520b02de8927a52b42193abc57dbba1882
JeffreyAes <aeschlimanjeffrey@gmail.com>
2022-11-02 18:15:59 -0700

    user crud

--- Python/flask_mysql/crud/users/server.py
+++ Python/flask_mysql/crud/users/server.py
@@ -11,12 +11,18 @@ def index():
 
 @app.route('/users')
 def users():
-    return render_template("read_all.html", users=User.get_all())
+    return render_template("users_all.html", users=User.get_all())
 
 
-@app.route("/create_user")
+@app.route("/users/create")
 def create_user():
     return render_template("create.html")
+
+
+@app.route("/users/<int:id>/show")
+def show_user(id):
+    one_user = User.get_one({"id": id})
+    return render_template("user_show_one.html", one_user=one_user)
 
 
 @app.route("/insert_user", methods=["POST"])
@@ -24,6 +30,33 @@ def addUser():
     User.save(request.form)
     return redirect('/users')
 
+
+@app.route("/users/<int:id>/delete")
+def delete_user(id):
+    data = {
+        'id': id
+    }
+    User.delete(data)
+    return redirect('/')
+
+
+@app.route("/users/<int:id>/edit")
+def edit_user(id):
+    data = {
+        'id':id
+    }
+
+    this_user = User.get_one(data)
+    return render_template("edit_user.html", this_user=this_user)
+
+@app.route("/users/<int:id>/update", methods=["POST"])
+def update_user(id):
+    data = {
+        **request.form,
+        'id':id
+    }
+    User.update(data)
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Python/flask_mysql/crud/users/user.py
+++ Python/flask_mysql/crud/users/user.py
@@ -1,4 +1,5 @@
 from mysqlconnections import connectToMySQL
+DATABASE = "users"
 
 
 class User:
@@ -14,7 +15,7 @@ class User:
     def get_all(cls):
         query = "SELECT * FROM users;"
 
-        results = connectToMySQL('users').query_db(query)
+        results = connectToMySQL(DATABASE).query_db(query)
 
         users = []
 
@@ -23,7 +24,34 @@ class User:
         return users
 
     @classmethod
+    def get_one(cls, data):
+        query = """
+        SELECT * FROM users WHERE id = %(id)s;
+        """
+        results = connectToMySQL(DATABASE).query_db(query, data)
+        if results:
+            return cls(results[0])
+        return False
+
+    @classmethod
     def save(cls, data):
         query = "INSERT INTO users ( first_name, last_name, email) VALUES  (%(first_name)s,%(last_name)s,%(email)s);"
-        result = connectToMySQL('users').query_db(query, data)
+        result = connectToMySQL(DATABASE).query_db(query, data)
+        return result
+
+    @classmethod
+    def delete(cls, data):
+        query = """
+            DELETE FROM users WHERE id = %(id)s;
+        """
+        result = connectToMySQL(DATABASE).query_db(query, data)
+        return result
+
+    @classmethod
+    def update(cls, data):
+        query = """
+        UPDATE users SET first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s
+        WHERE id = %(id)s;
+        """
+        result = connectToMySQL(DATABASE).query_db(query, data)
         return result
