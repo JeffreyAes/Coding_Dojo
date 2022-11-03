249de773e3ba209aa297ab409674f8dc3fae7dcc
JeffreyAes <aeschlimanjeffrey@gmail.com>
2022-11-02 21:59:03 -0700

    boilingplate

--- Python/flask_mysql/crud/boilerplate/flask_app/models/default_models.py
+++ Python/flask_mysql/crud/boilerplate/flask_app/models/default_models.py
@@ -5,6 +5,7 @@ DATABASE = "default"
 class Default:
     def __init__(self, data):
         self.id = data['id']
+        self.id = ['name']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 
@@ -19,36 +20,3 @@ class Default:
         for default in results:
             defaults.append(cls(default))
         return defaults
-
-    @classmethod
-    def get_one(cls, data):
-        query = """
-        SELECT * FROM default WHERE id = %(id)s;
-        """
-        results = connectToMySQL(DATABASE).query_db(query, data)
-        if results:
-            return cls(results[0])
-        return False
-
-    @classmethod
-    def save(cls, data):
-        query = "INSERT INTO default (default) VALUES (%(default)s);"
-        result = connectToMySQL(DATABASE).query_db(query, data)
-        return result
-
-    @classmethod
-    def delete(cls, data):
-        query = """
-            DELETE FROM default WHERE id = %(id)s;
-        """
-        result = connectToMySQL(DATABASE).query_db(query, data)
-        return result
-
-    @classmethod
-    def update(cls, data):
-        query = """
-        UPDATE default SET default = %(default)s
-        WHERE id = %(id)s;
-        """
-        result = connectToMySQL(DATABASE).query_db(query, data)
-        return result
