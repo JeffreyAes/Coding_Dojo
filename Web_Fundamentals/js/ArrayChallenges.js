72c9e664ef5ba17a88622602e1fb4ac6a47c72ce
JeffreyAes <aeschlimanjeffrey@gmail.com>
2022-11-09 18:59:54 -0800

    bvc

--- Web_Fundamentals/js/ArrayChallenges.js
+++ Web_Fundamentals/js/ArrayChallenges.js
@@ -74,5 +74,5 @@ function fibonacciArray(n) {
     return fibArr;
 }
 
-var result = fibonacciArray(10);
+var result = fibonacciArray(15);
 console.log(result); // we expect back [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
