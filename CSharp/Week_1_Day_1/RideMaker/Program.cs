0f383e78126198f33d39505389aed258aa699393
JeffreyAes <aeschlimanjeffrey@gmail.com>
2022-11-28 19:35:33 -0800

    Vehicles

---
+++ CSharp/Week_1_Day_1/RideMaker/Program.cs
@@ -0,0 +1,24 @@
+﻿Vehicle Horse = new Vehicle("Ed", 2, "brown", false, 20);
+System.Console.WriteLine(Horse.Name);
+
+Vehicle toyota = new Vehicle("toyota", 5, "red", true, 140);
+
+Vehicle Bike = new Vehicle("Bike", 2, "green", false, 20);
+
+Vehicle Truck = new Vehicle("Truck", 6, "Blue", true, 100);
+
+List<Vehicle> AllVehicles = new List<Vehicle>();
+AllVehicles.Add(Horse);
+AllVehicles.Add(toyota);
+AllVehicles.Add(Bike);
+AllVehicles.Add(Truck);
+
+foreach (Vehicle v in AllVehicles)
+{
+    v.ShowInfo();
+}
+
+Bike.Travel(100);
+Bike.Travel(100);
+
+Bike.ShowInfo();
