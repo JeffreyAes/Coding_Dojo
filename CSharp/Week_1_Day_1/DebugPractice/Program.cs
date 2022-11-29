6584fb1ae24ea700f2bc9ee33eab74634ed0f651
JeffreyAes <aeschlimanjeffrey@gmail.com>
2022-11-29 07:59:48 -0800

    whatever

--- CSharp/Week_1_Day_1/DebugPractice/Program.cs
+++ CSharp/Week_1_Day_1/DebugPractice/Program.cs
@@ -3,15 +3,15 @@
 bool amProgrammer = true;
 double Age = 27.9;
 List<string> Names = new List<string>();
-Names = "Monica";
+Names.Add("Monica");
 Dictionary<string, string> MyDictionary = new Dictionary<string, string>();
 MyDictionary.Add("Hello", "0");
 MyDictionary.Add("Hi there", "0");
 // This is a tricky one! Hint: look up what a char is in C#
-char MyName = "MyName";
+char MyName = 'N';
 // Challenge 2
 List<int> Numbers = new List<int>() {2,3,6,7,1,5};
-for(int i = Numbers.Count; i >= 0; i--)
+for(int i = Numbers.Count-1; i >= 0; i--)   
 {
     Console.WriteLine(Numbers[i]);
 }
@@ -27,17 +27,18 @@ foreach(int num in EvenMoreNumbers)
 {
     if(num % 3 == 0)
     {
-        num = 0;
+        EvenMoreNumbers[num] = 0;
     }
 }
 // Challenge 5
 // What can we learn from this error message?
 string MyString = "superduberawesome";
-MyString[7] = "p";
+MyString.Remove(7).Insert(7, "p");
+System.Console.WriteLine(MyString);
 // Challenge 6
 // Hint: some bugs don't come with error messages
 Random rand = new Random();
-int randomNum = rand.Next(12);
+int randomNum = rand.Next(13);
 if(randomNum == 12)
 {
     Console.WriteLine("Hello");
