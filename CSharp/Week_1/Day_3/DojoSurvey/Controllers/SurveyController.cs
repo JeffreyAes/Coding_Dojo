43547505d823181a1fa86b9c9a553481b8510f6f
JeffreyAes <aeschlimanjeffrey@gmail.com>
2022-11-30 22:15:29 -0800

    DojoSurvey

---
+++ CSharp/Week_1/Day_3/DojoSurvey/Controllers/SurveyController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DojoSurvey.Controllers;
+
+public class SurveyController : Controller
+{
+    [Route("")]
+    public ViewResult Index()
+    {
+        return View("Index");
+    }
+
+    [Route("/process")]
+    public IActionResult process(string name, string location, string language, string comment)
+    {
+    return RedirectToAction("ShowResults", new {name = name, location = location, language = language, comment = comment});
+
+    }
+
+    [Route("/results")]
+    public ViewResult ShowResults(string name, string location, string language, string comment)
+    {
+        ViewBag.Name = name;
+        ViewBag.Language = language;
+        ViewBag.Location = location;
+        ViewBag.Comment = comment;
+        return View("Results");
+    }
+
+
+
+}
