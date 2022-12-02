bcf40caa39725753bb823bb23386e936b91886cf
JeffreyAes <aeschlimanjeffrey@gmail.com>
2022-12-01 18:55:09 -0800

    dojomodel

---
+++ CSharp/Week_1/Day_4/DojoSurveyWModel/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyWModel.Models;
+
+namespace DojoSurveyWModel.Controllers;
+
+public class HomeController : Controller
+{
+    static Survey data;
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("")]
+    public ViewResult Index()
+    {
+        return View("Index");
+    }
+
+    [HttpPost("process")]
+    public IActionResult process(Survey newData)
+    {
+        data = newData;
+        return RedirectToAction("ShowResults");
+
+    }
+    [HttpGet("results")]
+    public ViewResult ShowResults()
+    {
+        
+        return View("Results", data);
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ CSharp/Week_1/Day_4/DojoSurveyWModel/Models/SurveyModel.cs
@@ -0,0 +1,8 @@
+namespace DojoSurveyWModel.Models;
+public class Survey
+{
+    public string Name {get;set;}
+    public string Location {get;set;}
+    public string Language {get;set;}
+    public string? Comment {get;set;}
+}
