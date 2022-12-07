5f28b3704838cbdd80feefb8c84428d468cb5d94
JeffreyAes <aeschlimanjeffrey@gmail.com>
2022-12-06 20:28:09 -0800

    CrudApplication

--- CSharp/Week_2/Day_2/CRUDelicious/Controllers/HomeController.cs
+++ CSharp/Week_2/Day_2/CRUDelicious/Controllers/HomeController.cs
@@ -7,26 +7,101 @@ namespace CRUDelicious.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    // Add a private variable of type MyContext (or whatever you named your context file)
+    private MyContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, MyContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
+    [HttpGet("")]
     public IActionResult Index()
     {
-        return View();
+        List<Dish> AllDishes = _context.Dishes.ToList();
+        return View("Index", AllDishes);
     }
 
-[HttpGet("dish/new")]
+    [HttpGet("dishes/new")]
     public IActionResult NewDish()
     {
         return View("NewDish");
+    }
+
+    [HttpPost("dishes/create")]
+    public IActionResult CreateDish(Dish newDish)
+    {
+        if (ModelState.IsValid)
+        {
+
+            _context.Add(newDish);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        else
+        {
+
+            return View("NewDish");
+        }
+    }
+
+    [HttpGet("Dishes/()dishId/show")]
+    public IActionResult ShowDish(int dishId)
+    {
+        Dish? DishToShow = _context.Dishes.FirstOrDefault(d => d.DishId == dishId);
+        if (DishToShow != null)
+        {
+            return View("OneDish", DishToShow);
+        }
+        else{
+            return RedirectToAction("Index");
+        }
+    }
+
+    [HttpPost("dishes/{dishId}/destroy")]
+    public IActionResult DestroyDish(int dishId)
+    {
+        Dish? DishToDestroy = _context.Dishes.SingleOrDefault(d =>  d.DishId == dishId);
+        _context.Dishes.Remove(DishToDestroy);
+        _context.SaveChanges();
+        return RedirectToAction("Index");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    [HttpGet("dishes/{dishId}/edit")]
+    public IActionResult EditDish(int dishId)
+    {
+        Dish? DishToEdit = _context.Dishes.FirstOrDefault(e => e.DishId == dishId);
+        if (DishToEdit != null)
+        {
+            return View("DishToEdit", DishToEdit);
+        } else {
+            return RedirectToAction("ShowDish");
+        }
+    }
+
+    [HttpPost("dishes/{dishId}/update")]
+    public IActionResult UpdateDish(Dish NewDish, int dishId)
+    {
+        if(ModelState.IsValid)
+        {
+            Dish?OldDish = _context.Dishes.FirstOrDefault(i => i.DishId == dishId);
+            OldDish.Name = NewDish.Name;
+            OldDish.Chef = NewDish.Chef;
+            OldDish.Tastiness = NewDish.Tastiness;
+            OldDish.Calories = NewDish.Calories;
+            OldDish.Description = NewDish.Description;
+            OldDish.Updated_At = DateTime.Now;
+            _context.SaveChanges();
+            return RedirectToAction("ShowDish", new{dishId=dishId});
+        }else{
+            return RedirectToAction("ShowDish");
+        }
     }
 }
--- CSharp/Week_2/Day_2/CRUDelicious/Models/Dish.cs
+++ CSharp/Week_2/Day_2/CRUDelicious/Models/Dish.cs
@@ -5,10 +5,17 @@ public class Dish
 {
     [Key]
     public int DishId { get; set; }
+    [Required]
     public string Name { get; set; }
+    [Required]
     public string Chef { get; set; }
-    public int Tastiness { get; set; }
-    public int Calories { get; set; }
+    [Required]
+    [Range(1,5)]
+    public int? Tastiness { get; set; }
+    [Required]
+    [Range(1,int.MaxValue)]
+    public int? Calories { get; set; }
+    [Required]
     public string Description { get; set; }
     public DateTime Created_At { get; set; } = DateTime.Now;
     public DateTime Updated_At { get; set; } = DateTime.Now;
