f8209797b6ad3c724c52cdcc0b9d21000cddb413
JeffreyAes <aeschlimanjeffrey@gmail.com>
2022-12-15 07:13:01 -0800

    some change i dunno

--- CSharp/Week_3/WeddingPlanner/Models/Guest.cs
+++ CSharp/Week_3/WeddingPlanner/Models/Guest.cs
@@ -8,8 +8,8 @@ public class Guest
     public int GuestId {get;set;}
 
     public int UserId {get;set;}
-    public User? User {get;set;}
     public int WeddingId {get;set;}
+    public User? User {get;set;}
     public Wedding? Wedding {get;set;}
 
     public DateTime CreatedAt {get;set;} = DateTime.Now;
