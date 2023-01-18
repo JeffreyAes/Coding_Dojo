809f1164675a3708b40e3c362042e4f4aa832f5d
JeffreyAes <aeschlimanjeffrey@gmail.com>
2023-01-17 22:05:17 -0800

    bruh

--- MERN/Week_2/Day_1/routing-practice/src/App.jsx
+++ MERN/Week_2/Day_1/routing-practice/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import './App.css';
 import { useState } from "react";
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import {
   Routes,
   Route,
@@ -14,50 +15,44 @@ const Home = (props) => {
     <div style={{ textAlign: "center" }} >
       <h1>Welcome.</h1>
       <div>
-
-      </div>
-      <div>
-
       </div>
     </div>
   );
 }
 
 const Four = (props) => {
+  const { num } = useParams()
+  const navigate = useNavigate()
+  if (isNaN(num))
+  {
+    navigate(Hello(num))
+  }
   return (
     <div style={{ textAlign: "center" }}>
-      <h1 >The magic number is 4</h1>
-      <div>
-
-      </div>
-      <div>
-
-      </div>
+      <h1 >The magic number is {num}</h1>
     </div>
   );
 }
 
 const Hello = (props) => {
-  
+  const { word } = useParams()
   const { color } = useParams()
   const { bgcolor } = useParams()
   return (
+    
     <div style={{ textAlign: "center" }}>
       <div style={{backgroundColor: bgcolor}}>
-      <h1 style={{color: color}}> the word is... hello </h1>
+      <h1 style={{color: color}}> the word is... {word} </h1>
       </div>
       <div>
         <p>try adding colors to the link. like hello/red/blue</p>
-
-      </div>
-      <div>
-
       </div>
     </div>
   )
 }
 
 function App() {
+  
   return (
     
     <div>
@@ -65,17 +60,17 @@ function App() {
       <nav >
         <ul className="NavThing">
           <li><Link to={"/"}>Home</Link></li>
-          <li><Link to={"/4"}>The Number Is</Link></li>
-          <li><Link to={"/hello"}>Hello</Link></li>
+          {/* <li><Link to={"/4"}>The Number Is</Link></li> */}
+          {/* <li><Link to={"/hello"}>Hello</Link></li> */}
         </ul>
       </nav>
       </div>
       <Routes>
 
-        <Route path="/hello/:color/:bgcolor" element={<Hello />} />
-        <Route path="/hello/:color" element={<Hello />} />
-        <Route path="/hello/" element={<Hello />} />
-        <Route path="/4" element={<Four />} />
+        <Route path="/:word/:color/:bgcolor" element={<Hello />} />
+        <Route path= "/:word/:color" element={<Hello />} />
+        <Route path="/:num" element={<Four />} />
+        <Route path= "/:word" element={<Hello />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </div>
