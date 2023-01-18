6c0fef1c7930608e8971bf8958c628ae82e1553f
JeffreyAes <aeschlimanjeffrey@gmail.com>
2023-01-18 10:25:43 -0800

    finally

--- MERN/Week_2/Day_1/routing-practice/src/App.jsx
+++ MERN/Week_2/Day_1/routing-practice/src/App.jsx
@@ -20,57 +20,67 @@ const Home = (props) => {
   );
 }
 
-const Four = (props) => {
-  const { num } = useParams()
-  // const navigate = useNavigate()
-  // if (isNaN(num))
-  // {
-  //   navigate(Hello(num))
-  // }
-  return (
-    <div style={{ textAlign: "center" }}>
-      <h1 >The magic number is {num}</h1>
-    </div>
-  );
-}
-
-const Hello = (props) => {
-  const { word } = useParams()
+const Thing = (props) => {
+  const { thing } = useParams()
   const { color } = useParams()
   const { bgcolor } = useParams()
-  return (
-    
-    <div style={{ textAlign: "center" }}>
-      <div style={{backgroundColor: bgcolor}}>
-      <h1 style={{color: color}}> the word is... {word} </h1>
+
+  if (isNaN(thing) == false ) {
+    return (
+
+      <div style={{ textAlign: "center" }}>
+        <h1 >The magic number is {thing}</h1>
       </div>
-      <div>
-        <p>try adding colors to the link. like hello/red/blue</p>
+    );
+  } else {
+    return (
+      <div style={{ textAlign: "center" }}>
+        <div style={{ backgroundColor: bgcolor }}>
+          <h1 style={{ color: color }}> the word is... {thing} </h1>
+        </div>
+        <div>
+          <p>try adding colors to the link. like hello/red/blue</p>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
+// const Word = (props) => {
+//   const { word } = useParams()
+//   const { color } = useParams()
+//   const { bgcolor } = useParams()
+//   return (
+
+//     <div style={{ textAlign: "center" }}>
+//       <div style={{ backgroundColor: bgcolor }}>
+//         <h1 style={{ color: color }}> the word is... {word} </h1>
+//       </div>
+//       <div>
+//         <p>try adding colors to the link. like hello/red/blue</p>
+//       </div>
+//     </div>
+//   )
+// }
+
 function App() {
-  
+
   return (
-    
+
     <div>
       <div >
-      <nav >
-        <ul className="NavThing">
-          <li><Link to={"/"}>Home</Link></li>
-          {/* <li><Link to={"/4"}>The Number Is</Link></li> */}
-          {/* <li><Link to={"/hello"}>Hello</Link></li> */}
-        </ul>
-      </nav>
+        <nav >
+          <ul className="NavThing">
+            <li><Link to={"/"}>Home</Link></li>
+          </ul>
+        </nav>
       </div>
       <Routes>
 
-        <Route path="/:word/:color/:bgcolor" element={<Hello />} />
-        <Route path= "/:word/:color" element={<Hello />} />
-        <Route path="/:num" element={<Four />} />
-        <Route path= "/:word" element={<Hello />} />
+        <Route path="/:thing/:color/:bgcolor" element={<Thing />} />
+        <Route path="/:thing/:color" element={<Thing />} />
+        <Route path="/:thing" element={<Thing />} />
+        {/* <Route path="/:word" element={<Word />} /> */}
         <Route path="/" element={<Home />} />
       </Routes>
     </div>
