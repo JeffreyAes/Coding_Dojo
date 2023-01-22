4eb27c3ad4a697a7ccaa9efd24c65e127a84432d
JeffreyAes <aeschlimanjeffrey@gmail.com>
2023-01-22 15:28:38 -0800

    product list

---
+++ MERN/Week_2/Day_5/product-manager/client/src/components/ProductList.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import axios from 'axios';
+import { Link } from 'react-router-dom'
+
+const ProductList = (props) => {
+    return (
+        <div>
+            {props.products.map((product, i) =>
+                <Link to={`/products/${product._id}`}
+                    key={i}>
+                    <p>
+                        {product.title}
+                    </p>
+                </Link>
+            )}
+        </div>
+    )
+}
+
+export default ProductList;
---
+++ MERN/Week_2/Day_5/product-manager/client/src/views/Detail.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { useParams, Link } from "react-router-dom";
+
+const Detail = (props) => {
+    const [product, setProduct] = useState({})
+    const { id } = useParams();
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/products/' + id)
+            .then(res => setProduct(res.data))
+            .catch(err => console.error(err));
+    }, []);
+
+    return (
+        <div>
+            <p>Title: {product.title}</p>
+            <p>Price: {product.price}</p>
+            <p>Description: {product.description}</p>
+            <div>
+                <Link to={'/products'}>Back</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Detail;
